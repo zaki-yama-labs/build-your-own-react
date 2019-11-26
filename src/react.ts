090313edb58438282ede1282700bfda14af8f1ea
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-11-26 21:11:43 +0900

    Step1: accept primitive types as children

--- src/react.ts
+++ src/react.ts
@@ -1,13 +1,25 @@
 export function createElement(
   type: string,
   props?: object | null,
-  ...children: object[]
+  ...children: any[]
 ) {
   return {
     type,
     props: {
       ...props,
-      children
+      children: children.map(child =>
+        typeof child === "object" ? child : createTextElement(child)
+      )
+    }
+  };
+}
+
+function createTextElement(text: string | number) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: []
     }
   };
 }
